53b626c5f21ffafe0191fca899827163b8df2c5f
Suzana Melo <suzanamelojor@gmail.com>
2024-03-10 20:58:00 +1300

    rekognition: create core rekognition service that receives image bytes and calls amazon rekognition for content moderation

---
+++ src/server/service/rekognition.ts
@@ -0,0 +1,59 @@
+import {
+  RekognitionClient,
+  DetectModerationLabelsCommand,
+  DetectModerationLabelsCommandInput,
+  DetectModerationLabelsCommandOutput,
+} from "@aws-sdk/client-rekognition";
+
+import { getConfig } from "../../utils/config";
+
+// get the AWS Rekognition client
+async function getClient() {
+  const config = getConfig();
+  const clientParams = {
+    region: config.aws.region as string,
+    credentials: {
+      accessKeyId: config.aws.accessKeyId as string,
+      secretAccessKey: config.aws.secretAccessKey as string,
+    },
+  };
+  const client = new RekognitionClient(clientParams);
+  return client;
+}
+
+// get the parameters for the content image
+function getParams(input: { imageBytes: Buffer }) {
+  let params: DetectModerationLabelsCommandInput;
+  if ("imageBytes" in input) {
+    params = {
+      Image: {
+        Bytes: input.imageBytes,
+      },
+    };
+  } else {
+    throw new Error("No imageBytes in input");
+  }
+  return params;
+}
+
+// send content image to AWS Rekognition
+async function moderateContentImage(
+  params: DetectModerationLabelsCommandInput
+): Promise<DetectModerationLabelsCommandOutput> {
+  const client = await getClient();
+  const command = new DetectModerationLabelsCommand(params);
+  const response = await client.send(command);
+  return response;
+}
+
+export async function getModerationContent(input: { imageBytes: Buffer }) {
+  try {
+    const params = getParams(input);
+    const response = await moderateContentImage(params);
+    const moderationLabels = response.ModerationLabels;
+    console.log("moderationLabels", moderationLabels);
+    return moderationLabels;
+  } catch (error) {
+    console.error("getModerationContent", error);
+  }
+}
